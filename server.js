c4a7996ccf0dd3293e571be3ffe477c28ebdf786
hawXinx <a1269039@unet.univie.ac.at>
2018-01-20 18:11:54 +0100

    Banking example

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const http = require('http')
-const express = require('express')
+const http = require('http');
+const express = require('express');
+var fs = require("fs");
+var bodyParser = require('body-parser');
 const port = 8080
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.post('/register', function(req, res) {
 	res.end('Hello World');
@@ -57,6 +62,31 @@ app.put('/user', function(req, res) {
 app.delete('/user', function(req, res) {
 	res.end('Hello World');
 	
+})
+
+app.get('/payment', function(req, res){
+	/*fs.readFile( "bankingWidget.html", 'utf8', function (err, data) {
+		
+	});*/
+	fs.readFile( "bankingExample1.json", 'utf8', function (err, data) {
+		res.end(data);
+	});
+})
+
+app.post('/payment', function(req, res){
+	//var bankname = req.body.bankname;
+	/*console.log("Bankname: " + bankname);
+	fs.readFile( "bankingThanks.html", 'utf8', function (err, data) {
+		var data2 = data.replace("BLANK", bankname);
+		res.end(data2);
+	});*/
+	fs.readFile("bankingExample1.json", 'utf8', function (err, data) {
+		var obj = JSON.parse(data);
+		obj.paymentMethod="CreditCard";
+		obj.paymentStatus="Done";
+		res.end(JSON.stringify(obj));
+	});
+
 })
 
 var server = app.listen(port, function(){
